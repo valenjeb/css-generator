9f36959e38bd27e067fd8720b233ab4ef5b0461a
valenjeb <7081274+valenjeb@users.noreply.github.com>
2024-06-03 00:42:43 +0300

    Fix testSupports did not perform any assertions

--- tests/CssGeneratorTest.php
+++ tests/CssGeneratorTest.php
@@ -33,11 +33,11 @@ class CssGeneratorTest extends TestCase
     {
         $css = CSS::new()
             ->selector(['html', 'body'])
-                ->display('block')
-                ->backgroundColor('#000')
+            ->display('block')
+            ->backgroundColor('#000')
             ->endSelector()
             ->selector('h1')
-                ->lineHeight(1)
+            ->lineHeight(1)
             ->endSelector();
 
         $expected = "html,\nbody {\n    display: block;\n    background-color: #000;\n}\n\nh1 {\n    line-height: 1;\n}";
@@ -49,9 +49,9 @@ class CssGeneratorTest extends TestCase
         $css = CSS::new();
         $css->media('(max-width: 768px)')
             ->selector(['html', 'body'])
-                ->display('block')
+            ->display('block')
             ->selector('h1')
-                ->lineHeight(1);
+            ->lineHeight(1);
 
         $expected = "@media (max-width: 768px) {\n    html,\n    body {\n        display: block;\n    }\n\n    h1 {\n        line-height: 1;\n    }\n}";
         $this->assertEquals($expected, trim($css->compile()));
@@ -61,20 +61,20 @@ class CssGeneratorTest extends TestCase
     {
         $css = CSS::new()
             ->selector(['html', 'body'])
-                ->display('block')
-                ->backgroundColor('#000000')
-                ->fontSize('1.6rem')
+            ->display('block')
+            ->backgroundColor('#000000')
+            ->fontSize('1.6rem')
             ->endSelector()
             ->selector('h1')
-                ->lineHeight(1)
+            ->lineHeight(1)
             ->endSelector();
 
         $css = CSS::new($css, ['indent' => 2])
             ->charset('utf-8')
             ->import('./style.css')->media('screen', 'print')->endImport()
             ->media('(max-width: 768px)')
-                ->selector(['html', 'body'])
-                    ->fontSize('1rem')
+            ->selector(['html', 'body'])
+            ->fontSize('1rem')
             ->endMedia();
 
         $minify   = false;
@@ -87,9 +87,9 @@ class CssGeneratorTest extends TestCase
     {
         $css = CSS::new()
             ->fontFace()
-                ->fontFamily('Arial')
-                ->src("url('path/to/arial.ttf')")
-                ->fontWeight('bold');
+            ->fontFamily('Arial')
+            ->src("url('path/to/arial.ttf')")
+            ->fontWeight('bold');
 
         $expected = "@font-face {\n    font-family: Arial;\n    src: url('path/to/arial.ttf');\n    font-weight: bold;\n}";
         $this->assertEquals($expected, trim($css->compile()));
@@ -99,16 +99,18 @@ class CssGeneratorTest extends TestCase
     {
         $css = CSS::new()
             ->supports('display: table-cell')
-                ->selector('body', ['font-size' => '16px'])
+            ->selector('body', ['font-size' => '16px'])
             ->endSupports()
             ->supports('display: table-cell')
             ->andSupports('display: list-item')
             ->andSupports('not (display: flex)')
             ->notSupports('display: contents')
-                ->media('screen')
-                    ->selector('body', ['font-size' => '16px'])
-                ->endMedia();
-        echo $css->compile();
-//        $this->assertEquals('', trim($css->compile()));
+            ->media('screen')
+            ->selector('body', ['font-size' => '16px'])
+            ->endMedia();
+        // echo $css->compile();
+        $expected = "@supports (display: table-cell) {\n    body {\n        font-size: 16px;\n    }\n}\n\n@supports (display: table-cell) and (display: list-item) and (not (display: flex)) not (display: contents) {\n@media screen {\n    body {\n        font-size: 16px;\n    }\n}\n}";
+
+        $this->assertEquals($expected, trim($css->compile()));
     }
 }
